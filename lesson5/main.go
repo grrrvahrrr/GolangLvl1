9b750481b7bcb63fb04509b125c05a92b1476cc5
Sergei <kabernik@gmail.com>
2021-10-06 16:21:19 +0300

    Lesson 5 HW

---
+++ lesson5/main.go
@@ -0,0 +1,84 @@
+//Lesson 5 Homework
+/*
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func fibb(x uint) uint {
+	if x <= 0 {
+		return 0
+	}
+	if x == 1 {
+		return 1
+	}
+	return fibb(x-1) + fibb(x-2)
+}
+
+
+func main() {
+	var num uint
+
+	fmt.Println(`Please input a number greater than "0"`)
+
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println(`Try again, something went wrong..`)
+		os.Exit(1)
+	}
+
+	if num < 0 {
+		fmt.Println(`Try again, something went wrong..`)
+		os.Exit(1)
+	}
+
+	fmt.Println(fibb(num))
+}
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func fibbMapCalc(x int) int {
+	fibbMap := make(map[int]int, x+1)
+
+	fibbMap[0] = 0
+	fibbMap[1] = 1
+
+	for i := 2; i < x+1; i++ {
+		fibbMap[i] = fibbMap[i-1] + fibbMap[i-2]
+	}
+
+	if x == 0 {
+		return fibbMap[0]
+	} else if x == 1 {
+		return fibbMap[1]
+	}
+
+	return fibbMap[x]
+}
+
+func main() {
+	var num int
+
+	fmt.Println(`Please input a number greater than "0"`)
+
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println(`Try again, something went wrong..`)
+		os.Exit(1)
+	}
+
+	if num < 0 {
+		fmt.Println(`Try again, something went wrong..`)
+		os.Exit(1)
+	}
+
+	fmt.Println(fibbMapCalc(num))
+}
