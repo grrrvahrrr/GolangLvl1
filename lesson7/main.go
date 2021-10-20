5ec4665c542b06bfa9d2b87655c79dcdb8505a0f
Sergei <kabernik@gmail.com>
2021-10-20 14:54:15 +0300

    Lesson 7 HW Update
    Переписал калькулятор с интерфейсами

--- lesson7/main.go
+++ lesson7/main.go
@@ -1,31 +1,15 @@
-//Lesson 7 Homework
-
 package main
 
 import (
 	"fmt"
-	"math"
 )
 
 const (
-	Addition       = "+"     //adding numbers
-	Subtraction    = "-"     //subtracting numbers
-	Multiplication = "*"     //multiplying numbers
-	Division       = "/"     //divising numbers
-	Squared        = "sqr"   //squared numbers
-	SqrRoot        = "root"  //square root
-	Logarithm      = "log"   //Logarithm of a number
-	Prime          = "prime" //Get prime numbers upto this point
+	Add     = "+"   //adding numbers
+	Sub     = "-"   //subtracting numbers
+	Multi   = "*"   //multiplying numbers
+	Squared = "sqr" //squared numbers
 )
-
-//Type Declaration
-type TwoNums struct {
-	num1, num2 float64
-}
-
-type OneNum struct {
-	num1 float64
-}
 
 //Error handling
 type Failure string
@@ -34,134 +18,102 @@ func (f Failure) Error() string {
 	return string(f)
 }
 
-//Interfaces
-type TwoNumInt interface {
-	ScanNums() error
-	Addition() float64
-	Subtraction() float64
-	Multiplication() float64
-	Division() float64
+//Structs
+type Addition struct {
+	num1, num2 float64
 }
 
-type OneNumInt interface {
-	ScanNum() error
-	Squared() float64
-	Sqrt() float64
-	Log() float64
+type Subtraction struct {
+	num1, num2 float64
 }
 
-//Two Number Functions
-func (nums *TwoNums) ScanNums() (err error) {
+type Multiplication struct {
+	num1, num2 float64
+}
+
+type Square struct {
+	num1 float64
+}
+
+//Number Scanners
+func Num2Scan() (a float64, b float64, err error) {
 	fmt.Println(`Input two numbers, please (divided by space)`)
-	_, err = fmt.Scanf("%f %f", &nums.num1, &nums.num2)
-	return Failure(`Try again, something went wrong.. One of nums is invalid`)
+	_, err = fmt.Scanf("%f %f", &a, &b)
+	return a, b, err
 }
 
-func (nums *TwoNums) Addition() float64 {
-	return nums.num1 + nums.num2
-}
-
-func (nums *TwoNums) Subtraction() float64 {
-	return nums.num1 - nums.num2
-}
-
-func (nums *TwoNums) Multiplication() float64 {
-	return nums.num1 * nums.num2
-}
-
-func (nums *TwoNums) Division() float64 {
-	return nums.num1 / nums.num2
-}
-
-//One number functions
-func (num *OneNum) ScanNum() (err error) {
+func Num1Scan() (a float64, err error) {
 	fmt.Println(`Input a number`)
-	_, err = fmt.Scanf("%f", &num.num1)
-	return Failure(`Try again, something went wrong.. Num is invalid`)
+	_, err = fmt.Scanf("%f", &a)
+	return a, err
 }
 
-func (num *OneNum) Squared() float64 {
-	return num.num1 * num.num1
+//Do functions
+type Operation interface {
+	Do() (float64, error)
 }
 
-func (num *OneNum) Sqrt() float64 {
-	return math.Sqrt(num.num1)
+func (a *Addition) Do() float64 {
+	return a.num1 + a.num2
 }
 
-func (num *OneNum) Log() float64 {
-	return math.Log(num.num1)
+func (s *Subtraction) Do() float64 {
+	return s.num1 - s.num2
+}
+
+func (m *Multiplication) Do() float64 {
+	return m.num1 * m.num2
+}
+
+func (s *Square) Do() float64 {
+	return s.num1 * s.num1
 }
 
 func main() {
+	fmt.Println(`Choose operation: "+" for Addition, "-" for Subtraction, "*" for Multiplication, "sqr" for squared value`)
+
 	var operation string
-
-	fmt.Println(`Choose operation: "+" for Addition, "-" for Subtraction, "*" for Multiplication, "/" for Division, "sqr" for Squared, "root" for Square root, "log" for Logarithm`)
-
 	_, err := fmt.Scan(&operation)
-	if err != nil {
-		err = Failure(`Try again, something went wrong.. Invalid Operation`)
-		panic(err)
-	}
 
 	switch operation {
-	case Addition:
-		var i TwoNumInt = &TwoNums{}
-		err = i.ScanNums()
+	case Add:
+		var a Addition
+		a.num1, a.num2, err = Num2Scan()
 		if err != nil {
+			err = Failure(`Try again, something went wrong..`)
 			panic(err)
 		}
-		fmt.Printf("Result: %.2f\n", i.Addition())
+		fmt.Printf("Result: %.2f\n", a.Do())
 
-	case Subtraction:
-		var i TwoNumInt = &TwoNums{}
-		err = i.ScanNums()
+	case Sub:
+		var s Subtraction
+		s.num1, s.num2, err = Num2Scan()
 		if err != nil {
+			err = Failure(`Try again, something went wrong..`)
 			panic(err)
 		}
-		fmt.Printf("Result: %.2f\n", i.Subtraction())
-
-	case Multiplication:
-		var i TwoNumInt = &TwoNums{}
-		err = i.ScanNums()
+		fmt.Printf("Result: %.2f\n", s.Do())
+	case Multi:
+		var m Multiplication
+		m.num1, m.num2, err = Num2Scan()
 		if err != nil {
+			err = Failure(`Try again, something went wrong..`)
 			panic(err)
 		}
-		fmt.Printf("Result: %.2f\n", i.Multiplication())
-
-	case Division:
-		var i TwoNumInt = &TwoNums{}
-		err = i.ScanNums()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Result: %.2f\n", i.Division())
+		fmt.Printf("Result: %.2f\n", m.Do())
 
 	case Squared:
-		var i OneNumInt = &OneNum{}
-		err = i.ScanNum()
+		var s Square
+		s.num1, err = Num1Scan()
 		if err != nil {
+			err = Failure(`Try again, something went wrong..`)
 			panic(err)
 		}
-		fmt.Printf("Result: %.2f\n", i.Squared())
-
-	case SqrRoot:
-		var i OneNumInt = &OneNum{}
-		err = i.ScanNum()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Result: %.2f\n", i.Sqrt())
-
-	case Logarithm:
-		var i OneNumInt = &OneNum{}
-		err = i.ScanNum()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Result: %.2f\n", i.Log())
+		fmt.Printf("Result: %.2f\n", s.Do())
 
 	default:
 		err = Failure(`Try again, something went wrong.. Invalid Operation`)
 		panic(err)
 	}
+
 }
